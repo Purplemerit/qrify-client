85b5f4898746601bce88ee4253cb7b034cb8a3dd
Avinash Poojary <avinashpoojary651@gmail.com>
2025-11-13 11:01:30 +0530

    register login updated

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -15,6 +15,8 @@ export interface SignupRequest {
 
 export interface SignupResponse {
   message: string;
+  accessToken: string;
+  refreshToken: string;
   user: User;
 }
 
@@ -82,6 +84,12 @@ class AuthService {
    */
   async signup(data: SignupRequest): Promise<SignupResponse> {
     const response = await api.post<SignupResponse>('/auth/signup', data);
+    const { accessToken, refreshToken } = response.data;
+
+    // Store tokens for immediate login
+    tokenStorage.setAccessToken(accessToken);
+    tokenStorage.setRefreshToken(refreshToken);
+
     return response.data;
   }
 
